c1a949aaac84ffddece7ec8d50b629a19eb98645
superclusterinit <supercluster.init@gmail.com>
2023-09-17 17:38:14 -0700

    added more styles and started confirming js functionality in browser and dom

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-const button = document.querySelector(".bears");
+/* function test() {
+  a = 5 + 5
+  console.log(a)
+}
+
+test();
+ */
+
+// Player selects an option
+// Cpu picks an option
+// Display the result
+// Manipulate the DOM and announce winner
+// Tally the score up to 5
+// Restart the game after a winner or button selected
+
+function game() {
+
+  playerScore = 0
+  dwightScore = 0
+
+  
 
 
-
-
+}
 
